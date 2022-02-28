6ca4b4baf129670c856d3dd376dd1cac872ce5f7
nickatillinois <nisse.degelin@hotmail.com>
2022-02-28 12:35:16 +0100

    Testfolder toegevoegd

--- src/main/java/swop/test.java
+++
@@ -1,5 +0,0 @@
-package swop;
-
-public class test {
-
-}
---
+++ src/test/swop/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package swop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+
+    @Test
+    void main() {
+
+    }
+}
