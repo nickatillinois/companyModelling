2d6b6fb48d70ea9f254e400a9a316c3e4d80a6bc
Nick2507 <nick.desmet1@student.kuleuven.be>
2022-03-08 16:03:46 +0100

    toevoegen enkele lege klassen

--- src/main/java/swop/Controller.java
+++ src/main/java/swop/Controller.java
@@ -1,6 +1,6 @@
 package swop;
 
-public class HelloWorld {
+public class Controller {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
---
+++ src/main/java/swop/Users.java
@@ -0,0 +1,4 @@
+package swop;
+
+public class Users {
+}
