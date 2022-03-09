ca648fe3ee489d80a708f3b3f1b8c668c1334100
nickatillinois <nisse.degelin@hotmail.com>
2022-03-09 12:10:02 +0100

    added junit

--- test/useCases/OrderNewCar1.java
+++ test/useCases/OrderNewCar1.java
@@ -22,7 +22,9 @@ public class OrderNewCar1 {
                 wheels);
         String modelName = "jaguar";
         CarModel carModel = new CarModel(modelName, cmf);
-        BodyEnum[] possibleValues = body.getBodyChoice().getDeclaringClass().getEnumConstants();
+        BodyEnum[] possibleValues = BodyEnum.values();
+
+
     }
 
 }
