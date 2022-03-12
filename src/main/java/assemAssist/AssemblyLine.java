fa4fffd056c2000a7df31e1d54ed985b5ce3873a
Nick2507 <nick.desmet1@student.kuleuven.be>
2022-03-12 21:46:59 +0100

    verder gewerkt aan ProductionScheduler en AssemblyLine

--- src/main/java/assemAssist/AssemblyLine.java
+++ src/main/java/assemAssist/AssemblyLine.java
@@ -1,5 +1,6 @@
 package assemAssist;
 
+import assemAssist.carOrder.CarOrder;
 import assemAssist.workStation.WorkStation;
 
 import java.util.ArrayList;
@@ -8,6 +9,10 @@ import java.util.List;
 public class AssemblyLine {
     private List<WorkStation> workStations;
     private CarManufacturingCompany carManufacturingCompany;
+    private int maxWorkingHoursToday = 22-6;
+    private int hoursWorkedToday = 0;
+    private final int STARTHOUR = 6;
+    private final int ENDHOUR = 22;
 
     public AssemblyLine(ArrayList<WorkStation> workStations, CarManufacturingCompany carManufacturingCompany){
         this.carManufacturingCompany = carManufacturingCompany;
@@ -29,4 +34,74 @@ public class AssemblyLine {
     public void setWorkStations(List<WorkStation> workStations) {
         this.workStations = workStations;
     }
+    public void nextDay(){
+        if(getHoursWorkedToday() <= ENDHOUR - STARTHOUR )
+            setMaxWorkingHoursToday(ENDHOUR - STARTHOUR);
+        else
+            setMaxWorkingHoursToday(ENDHOUR - STARTHOUR-((ENDHOUR - STARTHOUR) - getHoursWorkedToday()));
+        setHoursWorkedToday(0);
+    }
+
+    public int getMaxWorkingHoursToday() {
+        return maxWorkingHoursToday;
+    }
+
+    public void setMaxWorkingHoursToday(int maxWorkingHoursToday) {
+        this.maxWorkingHoursToday = maxWorkingHoursToday;
+    }
+
+    public int getHoursWorkedToday() {
+        return hoursWorkedToday;
+    }
+
+    public void setHoursWorkedToday(int hoursWorkedToday) {
+        this.hoursWorkedToday = hoursWorkedToday;
+    }
+    public List<CarOrder> getCurrentState(){
+        List<CarOrder> currentState  = new ArrayList<>(workStations.size());
+        for (int i = 0 ; i < workStations.size(); i++){
+            currentState.set(i, workStations.get(i).getCurrentOrder());
+        }
+        return currentState;
+
+    }
+    public int remainWorkingTime(){
+        return ENDHOUR-STARTHOUR-getHoursWorkedToday();
+    }
+
+    public boolean canMove(){
+        boolean canMove = true;
+        for (WorkStation workStation : workStations){
+            if(!workStation.isFinished()){
+                canMove = false;
+            }
+        }
+        return canMove;
+    }
+
+    public List<CarOrder> canNotMove(){
+        List<CarOrder> canNotMove = new ArrayList<>();
+        for(WorkStation workStation : workStations){
+            if (!workStation.isFinished())
+                canNotMove.add(workStation.getCurrentOrder());
+        }
+        return canNotMove;
+    }
+
+    public List<CarOrder> move(CarOrder carOrder, int timeBetweenToStates){
+        setHoursWorkedToday(getHoursWorkedToday() + timeBetweenToStates);
+        CarOrder finischedCar = workStations.get(2).getCurrentOrder();
+        finischedCar.setCompleted(true);
+        workStations.get(2).setCurrentOrder(workStations.get(1).getCurrentOrder());
+        workStations.get(1).setCurrentOrder(workStations.get(0).getCurrentOrder());
+        workStations.get(0).setCurrentOrder(carOrder);
+        List<CarOrder> newStateAndFiniched = new ArrayList<>(4);
+        newStateAndFiniched.set(0,carOrder);
+        newStateAndFiniched.set(1,workStations.get(1).getCurrentOrder());
+        newStateAndFiniched.set(2,workStations.get(2).getCurrentOrder());
+        newStateAndFiniched.set(3,finischedCar);
+        return newStateAndFiniched;
+    }
+
+
 }
--- src/main/java/assemAssist/ProductionScheduler.java
+++ src/main/java/assemAssist/ProductionScheduler.java
@@ -52,17 +52,41 @@ public ProductionScheduler(ArrayList<WorkStation> workStations,CarManufacturingC
             throw new IllegalArgumentException("The current carOrder can't be add to the Completed list because he is not completed!");
 
     }
-
-    public List<CarOrder> advanceAssemblyLine() {
-        //TODO
-        return null;
+    //maakt een simulatie
+    public List<CarOrder> simulateAdvanceAssemblyLine() {
+        List<CarOrder> currentState = getCurrentState();
+        List<CarOrder> nullList = new ArrayList<>(3);
+        nullList.set(0,null);
+        nullList.set(1,null);
+        nullList.set(2,null);
+        ArrayList<CarOrder> simulatedState = new ArrayList<>(3);
+        simulatedState.set(2, currentState.get(1));
+        simulatedState.set(1, currentState.get(0));
+        if (getProductionSchedule().size() == 0)
+            simulatedState.set(0,null);
+        else if(assemblyLine.remainWorkingTime() >=3 )
+            simulatedState.set(0, getProductionSchedule().get(0));
+        else
+            simulatedState.set(0,null);
+        if (simulatedState == nullList )
+            nextday();
+        return simulatedState;
     }
 
-    public List<CarOrder> advanceOrders(List<CarOrder> advanceAssemblyScheme){
-        //TODO
-        return getProductionSchedule();
+    //gaat de advance uitvoeren als het kan
+    public List<CarOrder> advanceOrders(int timeBetweenToStates){
+        if (!assemblyLine.canMove())
+            return null;
+        return assemblyLine.move(productionSchedule.remove(0),timeBetweenToStates).subList(0,2) ;
     }
-
+    public List<CarOrder> canNotMove(){
+        if (assemblyLine.canMove())
+            throw new IllegalCallerException("The caller can move!");
+        return assemblyLine.canNotMove();
+    }
+    public List<CarOrder> getCurrentState(){
+        return  assemblyLine.getCurrentState();
+    }
     public void addOrderToProductionSchedule(CarOrder carOrder){
         productionSchedule.add(carOrder);
 
@@ -70,4 +94,8 @@ public ProductionScheduler(ArrayList<WorkStation> workStations,CarManufacturingC
     public List<CarOrder> getOrdersFromGarageHolder(GarageHolder garageHolder){
         return productionSchedule.stream().filter(p->p.getGarageholder() == garageHolder).collect(Collectors.toList());
     }
+    public void nextday(){
+        assemblyLine.nextDay();
+    }
+
 }
