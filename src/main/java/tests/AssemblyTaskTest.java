4930ab7d1cda98070641a668de4d861acaa2dcd4
Luna Van den Bergh <luna.vandenbergh@student.kuleuven.be>
2022-03-15 15:13:21 +0100

    package tests + AssemblyTaskTest

---
+++ src/main/java/tests/AssemblyTaskTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+class AssemblyTaskTest {
+
+    @org.junit.jupiter.api.Test
+    void getIsCompleted() {
+        assert(true);
+    }
+
+    @org.junit.jupiter.api.Test
+    void setIsCompleted() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getComponent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWorkStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTaskDefinition() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+}
