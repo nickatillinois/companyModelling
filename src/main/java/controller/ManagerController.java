893478c855303db8a07cd39d525d47e09a9df0bc
RafSablon <raf.sablon@student.kuleuven.be>
2022-04-05 17:00:40 +0200

    Updated Manager UI

--- src/main/java/controller/ManagerController.java
+++ src/main/java/controller/ManagerController.java
@@ -2,6 +2,7 @@ package controller;
 
 import assemAssist.ProductionScheduler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ManagerController {
@@ -12,7 +13,7 @@ public class ManagerController {
         this.productionScheduler = productionScheduler;
     }
 
-    public List<List<String>> newLogin() {
+    /* public List<List<String>> newLogin() {
         return productionScheduler.getCurrentAndFutureStatusAndTaskStatus();
     }
 
@@ -20,7 +21,43 @@ public class ManagerController {
         List<String> MoveOrNot = productionScheduler.advanceOrders(currentPhaseTime);
 
         return MoveOrNot;
+    }*/
+
+    public List<String> checkStatistics() {
+        List<String> statistics = new ArrayList<>();
+        statistics.add("Average number of cars produced in a day: 10");
+        statistics.add("Median number of cars produced in a day: 11");
+        statistics.add("Cars produced yesterday: 13");
+        statistics.add("Cars produced two days ago: 9");
+
+        statistics.add("Average delay (in minutes): 7");
+        statistics.add("Median delay (in minutes): 5");
+        statistics.add("Last delay: 13 minutes, 3 days ago");
+        statistics.add("Second to last delay: 2 minutes, 5 days ago");
+
+        return statistics;
     }
 
+    public List<String> adaptSchedulingAlgorithm() {
+        List<String> availableSchedulingAlgorithms = new ArrayList<>();
+        availableSchedulingAlgorithms.add("FIFO");
+        availableSchedulingAlgorithms.add("Specification Batch");
 
+        return availableSchedulingAlgorithms;
+    }
+
+    public List<String> selectSchedulingAlgorithm(String algorithmName) {
+        if (!algorithmName.equals("Selection Batch")) {
+            List<String> batches = new ArrayList<>();
+            batches.add("Body, color, wheels");
+            batches.add("seats, Airco");
+            return batches;
+        } else {
+            return null;
+        }
+    }
+
+    public void selectBatchSet (String batchSet) {
+        return;
+    }
 }
--- src/main/java/ui/ManagerUI.java
+++ src/main/java/ui/ManagerUI.java
@@ -7,14 +7,15 @@ import java.util.Scanner;
 
 public class ManagerUI {
     private ManagerController managerController;
+    private Scanner in;
 
     public ManagerUI(ManagerController managerController) {
         this.managerController = managerController;
     }
 
     public void startUI(Scanner in) {
-
-        List<List<String>> currentAndFutureStatus = managerController.newLogin();
+        this.in = in;
+        /* List<List<String>> currentAndFutureStatus = managerController.newLogin();
         List<String> currentStatus = currentAndFutureStatus.get(0);
         List<String> futureStatus = currentAndFutureStatus.get(1);
         System.out.println("-------------");
@@ -29,11 +30,6 @@ public class ManagerUI {
                     System.out.println(modelOptions[j]);
                 }
 
-/*                System.out.println("Tasks:");
-                String[] tasksStatus = workstationStatus[1].split(", ");
-                for (int j = 0; j < tasksStatus.length; j++) {
-                    System.out.println(tasksStatus[j]);
-                }*/
             }
             else
                 System.out.println("No order at this work station.");
@@ -54,22 +50,13 @@ public class ManagerUI {
 
         List<String> statusAfterAdvance;
         while (true) {
-            System.out.println("Press \'a\' to advance the assembly line.");
-            String confirmation = in.next();
-            if (confirmation.equals("a")) {
-                while (true) {
-                    System.out.println("Enter the time spent during the current phase in hours:");
-                    try {
-                        int currentPhaseTime = Integer.parseInt(in.next());
-                        statusAfterAdvance = managerController.confirmAdvance(currentPhaseTime);
-                        break;
-                    } catch (NumberFormatException e) {
-                        System.out.println("This is not a valid time. Try again.");
-                    }
-                }
+            System.out.println("Enter the time spent during the current phase in hours:");
+            try {
+                int currentPhaseTime = Integer.parseInt(in.next());
+                statusAfterAdvance = managerController.confirmAdvance(currentPhaseTime);
                 break;
-            } else {
-                System.out.println("This is not a valid option. Try again.");
+            } catch (NumberFormatException e) {
+                System.out.println("This is not a valid time. Try again.");
             }
         }
 
@@ -91,15 +78,86 @@ public class ManagerUI {
                 }
             }
         }
+        */
 
         while (true) {
-            System.out.println("Press \'d\' when you are done viewing the status.");
-            String doneViewing = in.next();
-            if (doneViewing.equals("d")) {
-                System.out.println("Leaving the manager view.");
-                System.out.println("-------------");
-                return;
+            System.out.println("Press \'s\' to check the production statistics, press \'a\' to change the scheduling algorithm.");
+            String choice = in.next();
+            if (choice.equals("s")) {
+                statisticsUI();
+                break;
+            } else if (choice.equals("a")) {
+                algorithmUI();
+                break;
             } else {
+                System.out.println("This is not a valid option. Try again.");
+            }
+        }
+
+        System.out.println("-------------");
+        System.out.println("Leaving the manager view.");
+        System.out.println("-------------");
+
+    }
+
+    private void statisticsUI(){
+        List<String> statistics = managerController.checkStatistics();
+        for (String statistic : statistics) {
+            System.out.println(statistic);
+        }
+    }
+
+    private void algorithmUI() {
+        List<String> schedulingAlgorithms = managerController.adaptSchedulingAlgorithm();
+        System.out.println("The available scheduling algorithms are:");
+        for (int i = 0; i < schedulingAlgorithms.size(); i++) {
+            System.out.println(i + ". " + schedulingAlgorithms.get(i));
+        }
+
+        System.out.println("Select your chosen scheduling algorithm, by selecting a number. Press \'c\' to cancel.");
+        while (true) {
+            String chosenAlgorithm = in.next();
+            if (chosenAlgorithm.equals("c")) {
+                break;
+            } else {
+                try {
+                    int chosenAlgorithmNumber = Integer.parseInt(chosenAlgorithm);
+                    if (chosenAlgorithmNumber >= schedulingAlgorithms.size()) {
+                        System.out.println("This is not a valid option. Try again.");
+                    } else {
+                        List<String> notification = managerController.selectSchedulingAlgorithm(schedulingAlgorithms.get(chosenAlgorithmNumber));
+                        if (notification == null) {
+                            System.out.println("The scheduling algorithm has been changed to " + schedulingAlgorithms.get(chosenAlgorithmNumber));
+                        } else {
+                            specificationBatchUI(notification);
+                        }
+                        break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("This is not a valid option. Try again.");
+                }
+            }
+        }
+    }
+
+    private void specificationBatchUI(List<String> batches) {
+        System.out.println("The batches you can select are:");
+        for (int i = 0; i < batches.size(); i++) {
+            System.out.println(i + ". " + batches.get(i));
+        }
+
+        System.out.println("Select the set to batch process by typing the corresponding number.");
+        while (true) {
+            String chosenBatch = in.next();
+            try {
+                int chosenBatchNumber = Integer.parseInt(chosenBatch);
+                if (chosenBatchNumber >= batches.size()) {
+                    System.out.println("This is not a valid option. Try again.");
+                } else {
+                    managerController.selectBatchSet(batches.get(chosenBatchNumber));
+                    break;
+                }
+            } catch (NumberFormatException e) {
                 System.out.println("This is not a valid option. Try again.");
             }
         }
