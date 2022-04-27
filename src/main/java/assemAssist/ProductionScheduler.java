77baea229350f5ef296d86f33e21968f929a8e99
Luna Van den Bergh <luna.vandenbergh@student.kuleuven.be>
2022-04-27 04:16:21 +0200

    fix

--- src/main/java/assemAssist/ProductionScheduler.java
+++ src/main/java/assemAssist/ProductionScheduler.java
@@ -25,6 +25,7 @@ public class ProductionScheduler {
 private final AssemblyLine assemblyLine;
 private SchedulingAlgorithm schedulingAlgorithm = new FIFO();
 private final ArrayList<SchedulingAlgorithm> schedulers = new ArrayList<>();
+
     /**
      * Create a new production schedule af a single assembly line that can be managed by the manager
      */
@@ -35,6 +36,11 @@ public ProductionScheduler(){
 }
 
 
+    /**
+     * This function wil set the scheduling algorithme
+     * @param algorithm the algorithm you will set
+     * @throws IllegalArgumentException if the algorithm is not valid.
+     */
     public void selectSchedulingAlgorithm(String algorithm) throws IllegalArgumentException{
         for (SchedulingAlgorithm algorithm1 : schedulers )
             if (Objects.equals(algorithm, algorithm1.getName()))
@@ -88,28 +94,12 @@ public ProductionScheduler(){
             CarOrder carOrder = schedulingAlgorithm.getNextCarOrder();
             assemblyLine.move(carOrder, timeBetweenTwoStates);
         }
-    }
-
-    /**
-     * This function will add a real car order to the production schedule.
-     * @param carOrder | the car order added to the proction schedule
-     * @throws NullPointerException | car order is null
-     */
-    public LocalDate addOrderToProductionSchedule(CarOrder carOrder) throws NullPointerException, IllegalCompletionDateException, IllegalConstraintException, IllegalModelException {
-        if (carOrder != null) {
-            schedulingAlgorithm.addOrderToProductionSchedule(carOrder);
-            int carsToProduceToday = Math.min(assemblyLine.remainWorkingTime() - 2,0);
-            float carsToProduceLater = getProductionSchedule().size() -carsToProduceToday;
-            int daysNeeded = 0;
-            if (carsToProduceLater != 0 )
-                daysNeeded = (int)Math.ceil(carsToProduceLater/14);
-            return LocalDate.now().plusDays(daysNeeded);
-        }
-        else
-            throw new NullPointerException("You can not add null to the production schedule.");
+        if (Objects.equals(schedulingAlgorithm.getName(), "Specification Batch"))
+            if (schedulingAlgorithm.getProductionSchedule() == schedulers.get(0).getProductionSchedule())
+                selectSchedulingAlgorithm("FIFO");
 
     }
-
+    
     /**
      * This function return a list of carorders of the given garageHolder.
      * @param garageHolder | the garageholder of whom the carorders are to be found
@@ -154,6 +144,10 @@ public ProductionScheduler(){
         this.schedulingAlgorithm = schedulingAlgorithm;
     }
 
+    /**
+     * Gives a list of pending orders in the work stations.
+     * @return the pending orders in the workstations of the assembly line
+     */
     public List<CarOrder> getPendingOrders(){
         ArrayList<CarOrder> pending = new ArrayList<>();
         for (WorkStation workStation : getAssemblyLine().getWorkStations())
--- src/main/java/controller/GarageHolderController.java
+++ src/main/java/controller/GarageHolderController.java
@@ -1,25 +1,36 @@
 package controller;
 
+<<<<<<< HEAD
+=======
+import assemAssist.Company;
+>>>>>>> parent of a6819f7 (Revert "Merge remote-tracking branch 'origin/main'")
 import assemAssist.ProductionScheduler;
 import assemAssist.exceptions.IllegalChoiceException;
 import assemAssist.exceptions.IllegalCompletionDateException;
 import assemAssist.exceptions.IllegalModelException;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 public class GarageHolderController {
 
+<<<<<<< HEAD
     private final ProductionScheduler productionScheduler;
+=======
+>>>>>>> parent of a6819f7 (Revert "Merge remote-tracking branch 'origin/main'")
     private String garageHolder;
     private String chosenModel;
+    private Company company;
 
 
     public GarageHolderController(ProductionScheduler productionScheduler) {
         this.productionScheduler = productionScheduler;
     }
-    public List<List<String>> newLogin(String garageHolder) {
+    public ArrayList<String[]>[] newLogin(String garageHolder) {
         this.garageHolder = garageHolder;
+<<<<<<< HEAD
         return this.productionScheduler.newLogin(garageHolder);
     }
     public List<String> wantsToOrder() {
@@ -35,6 +46,21 @@ public class GarageHolderController {
 
     public String completeOrderingForm(List<String> carOptions) throws IllegalChoiceException, IllegalModelException, IllegalCompletionDateException {
         return productionScheduler.completeOrderingForm(carOptions, this.garageHolder, this.chosenModel).toString();
+=======
+        return this.company.newLogin(garageHolder);
+    }
+    public HashSet<String> wantsToOrder() {
+        return company.getAvailableModels();
+    }
+
+    public Map<String, HashSet<String>> selectModel(String carModel) throws IllegalModelException {
+        this.chosenModel = carModel;
+        return company.selectModel(carModel);
+    }
+
+    public String completeOrderingForm(List<String> carOptions) throws IllegalChoiceException, IllegalModelException, IllegalCompletionDateException {
+        return company.completeOrderingForm((Map<String, String>) carOptions, this.garageHolder, this.chosenModel);
+>>>>>>> parent of a6819f7 (Revert "Merge remote-tracking branch 'origin/main'")
     }
 
     public List<String> viewDetails(int carID, String garageHolderName) throws IllegalArgumentException {
