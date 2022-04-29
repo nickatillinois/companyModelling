3c56e277523b62f9503c5b6336e3a55453dc021b
Luna Van den Bergh <luna.vandenbergh@student.kuleuven.be>
2022-04-29 15:17:44 +0200

    use cases start

--- src/test/java/useCases/CheckProductionStatisticsTest.java
+++ src/test/java/useCases/CheckProductionStatisticsTest.java
@@ -17,18 +17,18 @@ import java.util.TreeMap;
 
 public class CheckProductionStatisticsTest {
 
+    static Company company;
+    static AssemblyLine assemblyLine;
+    static ProductionScheduler productionScheduler;
+    static Mechanic mechanic;
+
     @BeforeAll
-    static void init() {
+    static void init() throws IllegalCompletionDateException, IllegalConstraintException, IllegalModelException, OptionThenComponentException, OptionAThenOptionBException, RequiredComponentException {
         ByteArrayInputStream in = new ByteArrayInputStream("3\ns".getBytes());
         System.setIn(in);
-    }
-
-    @Test
-    public void checkProductionStatistics() throws IllegalCompletionDateException, IllegalModelException, IllegalConstraintException, OptionThenComponentException, OptionAThenOptionBException, RequiredComponentException {
-
-        Company company = new Company();
-        ProductionScheduler productionScheduler = company.getProductionScheduler();
-        AssemblyLine assemblyLine = productionScheduler.getAssemblyLine();
+        company = new Company();
+        productionScheduler = company.getProductionScheduler();
+        assemblyLine = productionScheduler.getAssemblyLine();
         TreeMap<String, String> legalAOptions = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
         legalAOptions.put("color", "red"); legalAOptions.put("body", "break");
         legalAOptions.put("engine", "v4"); legalAOptions.put("seats", "leather white");
@@ -39,17 +39,16 @@ public class CheckProductionStatisticsTest {
         company.completeOrderingForm(legalAOptions,"Luna Van den Bergh","A");
         company.completeOrderingForm(legalAOptions,"Luna Van den Bergh","A");
 
-        // TODO complete alle tasks telkens
+    }
+
+    @Test
+    public void checkProductionStatistics() throws IllegalCompletionDateException, IllegalModelException, IllegalConstraintException, OptionThenComponentException, OptionAThenOptionBException, RequiredComponentException {
 
 
-        productionScheduler.advanceOrders(60);
-        productionScheduler.advanceOrders(60);
-        productionScheduler.advanceOrders(60); // first one done: delay = 0
-        productionScheduler.advanceOrders(90); // second one done: delay = 30
-        productionScheduler.advanceOrders(70); // third one done: delay = 40
-        new UI( new GarageHolderUI( new GarageHolderController(company)),
+
+    /*    new UI( new GarageHolderUI( new GarageHolderController(company)),
                 new ManagerUI(      new ManagerController(company)),
                 new MechanicUI(     new MechanicController( new Mechanic(assemblyLine) )));
-
+    */
     }
 }
--- src/test/java/useCases/PerformAssemblyTasksTest.java
+++ src/test/java/useCases/PerformAssemblyTasksTest.java
@@ -1,35 +1,51 @@
 package useCases;
 
-import assemAssist.AssemblyLine;
-import assemAssist.Company;
-import assemAssist.ProductionScheduler;
-import assemAssist.exceptions.IllegalCompletionDateException;
-import assemAssist.exceptions.IllegalModelException;
+import assemAssist.*;
+import assemAssist.exceptions.*;
+import controller.GarageHolderController;
+import controller.ManagerController;
+import controller.MechanicController;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import ui.GarageHolderUI;
+import ui.ManagerUI;
+import ui.MechanicUI;
+import ui.UI;
 
 import java.io.ByteArrayInputStream;
+import java.util.TreeMap;
 
 public class PerformAssemblyTasksTest {
 
+    static Company company;
+    static AssemblyLine assemblyLine;
+    static ProductionScheduler productionScheduler;
+    static Mechanic mechanic;
+
     @BeforeAll
-    static void init() {
-        ByteArrayInputStream in = new ByteArrayInputStream("3\ns".getBytes());
+    static void init() throws IllegalCompletionDateException, IllegalConstraintException, IllegalModelException, OptionThenComponentException, OptionAThenOptionBException, RequiredComponentException {
+        ByteArrayInputStream in = new ByteArrayInputStream("1\nw\n0\n0\nd\n60\n0\nd\n60\n4".getBytes());
         System.setIn(in);
+        company = new Company();
+        productionScheduler = company.getProductionScheduler();
+        assemblyLine = productionScheduler.getAssemblyLine();
+        TreeMap<String, String> legalAOptions = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+        legalAOptions.put("color", "red"); legalAOptions.put("body", "break");
+        legalAOptions.put("engine", "v4"); legalAOptions.put("seats", "leather white");
+        legalAOptions.put("airco", "manual"); legalAOptions.put("gearbox", "6 manual");
+        legalAOptions.put("wheels", "winter");
+        company.completeOrderingForm(legalAOptions,"Luna Van den Bergh","A");
+        company.completeOrderingForm(legalAOptions,"Luna Van den Bergh","A");
+        company.completeOrderingForm(legalAOptions,"Luna Van den Bergh","A");
+        company.completeOrderingForm(legalAOptions,"Luna Van den Bergh","A");
+        mechanic = new Mechanic(assemblyLine);
     }
 
     @Test
-    public void checkProductionStatistics() throws IllegalCompletionDateException, IllegalModelException {
-        ProductionScheduler productionScheduler = new ProductionScheduler();
-        Company company = new Company();
-        //CarModel model = new CarModel();
-        //CarOrder orderA = new CarOrder();
-        //company.setCompletedCarOrders();
-        AssemblyLine assemblyLine = new AssemblyLine();
-/*        new UI( new GarageHolderUI( new GarageHolderController(company)),
+    public void performAssemblyTasks() throws IllegalCompletionDateException, IllegalModelException, IllegalConstraintException, OptionThenComponentException, OptionAThenOptionBException, RequiredComponentException {
+        new UI( new GarageHolderUI( new GarageHolderController(company)),
                 new ManagerUI(      new ManagerController(company)),
-                new MechanicUI(     new MechanicController( new Mechanic(productionScheduler,assemblyLine) )));
-*/
+                new MechanicUI(     new MechanicController( mechanic )));
     }
 
 }
